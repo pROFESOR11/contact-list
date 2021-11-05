2ae2dcfad9577e401123e55ea42ec7c1d098e281
pROFESOR11 <ali_cimen_11@hotmail.com>
2021-11-05 19:00:34 +0300

    add homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,43 @@
-export const Home = (): JSX.Element => <div>Placeholder</div>
+import { GetServerSideProps } from 'next'
+import React from 'react'
+
+import Contacts from '@components/Contacts'
+import Layout from '@components/Layout'
+import { ContactWithTags } from '@customTypes/ContactWithTags'
+import useContacts from '@hooks/useContacts'
+import prisma from '@lib/prisma'
+
+interface HomeProps {
+  contacts: ContactWithTags[]
+}
+
+export const Home: React.FC<HomeProps> = ({ contacts: initialContacts }) => {
+  const [contacts, methods] = useContacts(initialContacts)
+
+  return (
+    <Layout>
+      <Layout.Header />
+      <Layout.Content>
+        <Contacts contacts={contacts} methods={methods} />
+      </Layout.Content>
+      <Layout.Footer />
+    </Layout>
+  )
+}
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const contacts = await prisma.contact.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+    include: {
+      tags: true,
+    },
+  })
+
+  return {
+    props: { contacts },
+  }
+}
