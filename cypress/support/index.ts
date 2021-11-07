73c2ea469a82d1d3d6a4a26990c9790faa9ab58c
pROFESOR11 <ali_cimen_11@hotmail.com>
2021-11-07 16:27:10 +0300

    add custom support for getByTestId

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,3 +1,6 @@
+// load the global Cypress types
+/// <reference types="cypress" />
+
 // ***********************************************************
 // This example support/index.js is processed and
 // loaded automatically before your test files.
@@ -13,8 +16,6 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-// Import commands.js using ES2015 syntax:
-import './commands'
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+Cypress.Commands.add('getByTestId', (selector, ...args) => {
+  return cy.get(`[data-testid=${selector}]`, ...args)
+})
